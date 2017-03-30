71833c035a163ba3182f0cf01058a7afe16aceb7
Mark Pedrotti <pedrottimark@gmail.com>
2017-03-30 13:46:00 -0400

    Add comment to slice example

--- 2017-03-30/array/13-slice.js
+++ 2017-03-30/array/13-slice.js
@@ -4,6 +4,7 @@ console.log('array before slice:', array);
 
 // from the start index
 // to (but not including) the end index
+// if omitted, the end index is the array length
 // number of sliced items is end - start
 var sliced = array.slice(1, 3);
 
