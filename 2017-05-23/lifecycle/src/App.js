60ae24c05cce597051ab9678f42f0bd331c16d94
Matt Stegall <mstegall928@gmail.com>
2017-05-23 17:10:14 -0400

    Lifecycle testbed

---
+++ 2017-05-23/lifecycle/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+import Lifecycle from "./Lifecycle"
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: "test",
+      displayValue: "test2",
+      showDisplay: true,
+    }
+    this.onSet = this.onSet.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.onToggle = this.onToggle.bind(this)
+  }
+
+  onSet() {
+    this.setState(prev => Object.assign({}, prev, {
+      displayValue: prev.inputValue
+    }))
+  }
+
+  onToggle() {
+    this.setState(prev => Object.assign({}, prev, {
+      showDisplay: !prev.showDisplay
+    }))
+  }
+
+  onChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Lifecycle demo</h1>
+        <input type="text" value={this.state.inputValue} onChange={this.onChange} />
+        <button onClick={this.onSet}>Set Display</button>
+        <button onClick={this.onToggle}>Toggle Display</button>
+        {this.state.showDisplay && <Lifecycle message={this.state.displayValue}/>}
+      </div>
+    );
+  }
+}
+
+export default App;
